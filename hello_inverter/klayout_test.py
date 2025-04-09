d51bae3f53a56bf0a06d7a3b5ac78e0008895822
jjdehl <jacobjdehlsen@gmail.com>
2025-04-09 16:58:57 +0200

    updates wed

--- hello_inverter/klayout_test.py
+++ hello_inverter/klayout_test.py
@@ -35,8 +35,29 @@ for layer in lw.each_layer():
 layout = pya.Layout()
 top = layout.create_cell("t")
 m1 = layout.layer(68, 20) # metal1
+m1label = layout.layer(68, 5) # metal1 label
+
+m2 = layout.layer(69, 20) # metal2
+m2pin = layout.layer(69, 16) # metal2 pin
+m2label = layout.layer(69, 5) # metal2 label
+
+via2 = layout.layer(69, 44) # via2
+
+m3 = layout.layer(70, 20) # metal3
+via3 = layout.layer(70, 44) # via3
+
+m4 = layout.layer(71, 20) # metal4
+m4pin = layout.layer(71, 16) # metal4 pin
+m4label = layout.layer(71, 5) # metal4 label
+
+via1 = layout.layer(68, 44) # via1
+
+
 m1pin = layout.layer(68,16)
+
 bd = layout.layer(235, 4) # boundary
+
+mc = layout.layer(81, 2) # memcore
 
 li1 = layout.layer(67, 20)
 li1pin = layout.layer(67, 16)
@@ -48,29 +69,55 @@ mcon1 = layout.layer(67, 44)
 
 # Bounding box
 top.shapes(bd).insert(pya.Box(0, 0, 10000, 5000))
-
+top.shapes(mc).insert(pya.Box(0, 0, 10000, 5000))
 
 # Metal1 layer with interconnect
 top.shapes(m1).insert(pya.Box(0, 0, 1000, 2000))
 top.shapes(m1pin).insert(pya.Box(0, 0, 1000, 2000))
+top.shapes(m2).insert(pya.Box(0, 0, 1000, 2000))
+top.shapes(m2pin).insert(pya.Box(0, 0, 1000, 2000))
+top.shapes(via1).insert(pya.Box(100, 100, 900, 1900))
+
 
 top.shapes(m1).insert(pya.Box(8000, 0, 10000, 2000))
 top.shapes(m1pin).insert(pya.Box(8000, 0, 10000, 2000))
-
+top.shapes(m2).insert(pya.Box(8000, 0, 10000, 2000))
+top.shapes(m2pin).insert(pya.Box(8000, 0, 10000, 2000))
+top.shapes(via1).insert(pya.Box(8100, 100, 9900, 1900))
 
 top.shapes(m1).insert(pya.Box(8000, 3000, 10000, 5000))
 top.shapes(m1pin).insert(pya.Box(8000, 3000, 10000, 5000))
+top.shapes(m2).insert(pya.Box(8000, 3000, 10000, 5000))
+#top.shapes(m2pin).insert(pya.Box(8000, 3000, 10000, 5000))
+top.shapes(via1).insert(pya.Box(8100, 3100, 9900, 4900))
+top.shapes(via2).insert(pya.Box(8100, 3100, 9900, 4900))
+top.shapes(m3).insert(pya.Box(8000, 3000, 10000, 5000))
+top.shapes(via3).insert(pya.Box(8100, 3100, 9900, 4900))
+top.shapes(m4).insert(pya.Box(8000, 3000, 10000, 5000))
+top.shapes(m4pin).insert(pya.Box(8000, 3000, 10000, 5000))
+#top.shapes(m4label).insert(pya.Box(8000, 3000, 10000, 5000))
+
 
 top.shapes(m1).insert(pya.Box(0, 3000, 1000, 5000))
 top.shapes(m1pin).insert(pya.Box(0, 3000, 1000, 5000))
+top.shapes(m2).insert(pya.Box(0, 3000, 1000, 5000))
+#top.shapes(m2pin).insert(pya.Box(0, 3000, 1000, 5000))
+top.shapes(via1).insert(pya.Box(100, 3100, 900, 4900))
+top.shapes(via2).insert(pya.Box(100, 3100, 900, 4900))
+top.shapes(m3).insert(pya.Box(0, 3000, 1000, 5000))
+top.shapes(via3).insert(pya.Box(100, 3100, 900, 4900))
+top.shapes(m4).insert(pya.Box(0, 3000, 1000, 5000))
+top.shapes(m4pin).insert(pya.Box(0, 3000, 1000, 5000))
+
+
 
 top.shapes(li1).insert(pya.Box(8000, 3000, 10000, 5000))
 top.shapes(mcon1).insert(pya.Box(8100, 3100, 9900, 4900))
-top.shapes(li1pin).insert(pya.Box(8000, 3000, 10000, 5000))
+#top.shapes(li1pin).insert(pya.Box(8000, 3000, 10000, 5000))
 
 top.shapes(li1).insert(pya.Box(0, 3000, 1000, 5000))
 top.shapes(mcon1).insert(pya.Box(100, 3100, 900, 4900))
-top.shapes(li1pin).insert(pya.Box(0, 3000, 1000, 5000))
+#top.shapes(li1pin).insert(pya.Box(0, 3000, 1000, 5000))
 
 
 
@@ -86,21 +133,21 @@ top.shapes(m1).insert(pin_box2)
 
 # Add text label at the center of the box
 text = pya.Text("A", pya.Trans(pin_pos))
-top.shapes(m1).insert(text)
+top.shapes(m2label).insert(text)
 
 text = pya.Text("Y", pya.Trans(pin_pos2))
-top.shapes(m1).insert(text)
+top.shapes(m2label).insert(text)
 
 pin_pos3 = pya.Point(500, 3500)
 text = pya.Text("VPWR", pya.Trans(pin_pos3))
-top.shapes(m1).insert(text)
-top.shapes(li1).insert(text)
+top.shapes(m4label).insert(text)
 
 pin_pos4 = pya.Point(8500, 3500)
 text = pya.Text("VGND", pya.Trans(pin_pos4))
-top.shapes(m1).insert(text)
-top.shapes(li1).insert(text)
+top.shapes(m4label).insert(text)
+#top.shapes(li1).insert(text)
 
+# Add pin label
 
 layout.write("t.gds")
 print("Completed layout creation.")
